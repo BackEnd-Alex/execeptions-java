38915f92fb69499897bcc1a1cf16dc0c5fbb3490
Alexsandro <contadetestealexsandro@gmail.com>
2024-02-09 11:40:41 -0200

    Add files via upload
    Solução muito ruim.

---
+++ Exececoes/src/application/Main.java
@@ -0,0 +1,57 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner leia = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.print("Room number: ");
+        int roomNumber = leia.nextInt();
+
+        System.out.print("Check-in date(dd/MM/yyyy) : ");
+        leia.nextLine();
+        Date checkin = sdf.parse(leia.next());
+
+        System.out.print("Check-out date(dd/MM/yyyy) : ");
+        leia.nextLine();
+
+        Date checkout = sdf.parse(leia.next());
+        if (!checkout.after(checkin)){
+            System.out.println("Erro na reserva, data de saída menor que a de entrada.");
+        }
+
+        else {
+            Reservation reservation = new Reservation(roomNumber,checkin,checkout);
+            System.out.println("Reservation: "+ reservation);
+        }
+        Reservation reservation = new Reservation(roomNumber,checkin,checkout);
+        System.out.println();
+        System.out.println("Enter data to update the reservation: ");
+
+        System.out.print("Check-in date(dd/MM/yyyy) : ");
+        checkin = sdf.parse(leia.next());
+
+        System.out.print("Check-out date(dd/MM/yyyy) : ");
+        checkout = sdf.parse(leia.next());
+
+        Date now = new Date();
+        if(checkin.before(now) || checkout.before(now)){
+
+            System.out.println("Reservation dates for update must be future dates.");
+        }
+        else if (!checkout.after(checkin)) {
+            System.out.println("Check-out date must be after check-in date.");
+        }
+        else {
+
+            reservation.upDates(checkin, checkout);
+            System.out.println("Reservation: "+ reservation);
+        }
+    }
+
+}
---
+++ Exececoes/src/model/entities/Reservation.java
@@ -0,0 +1,65 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkin;
+    private Date checkout;
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    public Reservation(){
+
+    }
+    public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+        this.roomNumber = roomNumber;
+        this.checkin = checkin;
+        this.checkout = checkout;
+    }
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public Date getCheckin() {
+        return checkin;
+    }
+    public void setCheckin(Date checkin) {
+        this.checkin = checkin;
+    }
+    public Date getCheckout() {
+        return checkout;
+    }
+    public void setCheckout(Date checkout) {
+        this.checkout = checkout;
+    }
+    public long duration(){
+             //CALCULO DE DATA EM MILESEGUNDO. DIFF É UM NOME QUALQUER PARA A VAR long.
+    long diff = checkout.getTime() - checkin.getTime();
+
+            //TRANSFORMA MILLISECONDS EM DIAS.
+        return   TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);
+
+    }
+    public void upDates(Date checkin, Date checkout){
+        this.checkin = checkin;
+        this.checkout = checkout;
+
+    }
+
+    @Override
+    public String toString() {
+        return  "Room: "
+                + roomNumber
+                +", checkin: "
+                + sdf.format(checkin)
+                + ", checkout: "
+                + sdf.format(checkout)
+                + ", "
+                +  duration()
+                + " Nigths";
+            }
+}
+
